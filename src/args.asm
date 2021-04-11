3502d569c64359692f4a087f957fd2fae8110a30
Chris Mounce <christophermounce@gmail.com>
2021-04-11 02:01:58 -0700

    Minor refactoring and cleanup of arg parser

--- src/args.asm
+++ src/args.asm
@@ -79,6 +79,7 @@ parsed_args_size equ $-parsed_args
 ; Subroutines
 ;------------------------------
 segment .text
+%include "debug.asm"
 
 ; The tokenization code in this section uses SI and CX to point to each token
 ; in the parsed command line. SI points to the first byte of the current token,
@@ -94,7 +95,6 @@ segment .text
 ; end. It takes no parameters and returns nothing: it just mutates the global
 ; variables to match what the command line args specify.
 ;-------------------------------------------------------------------------------
-%include "debug.asm"
 parse_command_line:
 ; Clear memory
 mov cx, parsed_args_size
@@ -103,11 +103,20 @@ mov al, 0
 rep stosb
 
 ; Parse the first token as an optional subcommand
-mov cx, 0                   ; Prepare to parse
-call parse_next_token
-call try_read_subcommand
+call parse_first_token
+call try_consume_subcommand
 mov al, [args_subcommand]
 inspect "Subcommand enum:", al
+
+; Cycle through the remaining tokens
+cmp cx, 0
+je .ret
+.loop:
+call parse_next_token
+cmp cx, 0
+je .ret
+inspect "Parsed token of length", cl
+jmp .loop
 
 ; TODO: would it be better if there was a single consume_token command?
 ;   1. If it begins with a /, it tries to parse it as a flag
@@ -122,16 +131,6 @@ inspect "Subcommand enum:", al
 ; Also TODO: Who should handle printing the error?
 ; Maybe we just return AL=status and SI/CX the relevant string
 
-
-cmp cx, 0
-je .ret
-.loop:
-call parse_next_token
-cmp cx, 0
-je .ret
-inspect "Parsed token of length", cl
-jmp .loop
-
 .ret:
 ret
 
@@ -139,7 +138,7 @@ ret
 ;-------------------------------------------------------------------------------
 ; Sets args_subcommand based on the token currently pointed to by SI/CX
 ;-------------------------------------------------------------------------------
-try_read_subcommand:
+try_consume_subcommand:
 mov [args_subcommand], byte SUBCOMMAND_PREVIEW  ; Default subcommand if none is provided
 cmp cx, 0               ; No token to read
 je .ret
@@ -168,6 +167,7 @@ mov dl, [di]
 mov [args_subcommand], dl
 .restore:
 pop di
+call parse_next_token
 .ret:
 ret
 
@@ -216,6 +216,17 @@ add al, ('a' - 'A')
 ret
 
 ;-------------------------------------------------------------------------------
+; Sets up SI/CX to point to the first token in the argument string.
+;
+; Returns CX=0 if there are no tokens.
+;-------------------------------------------------------------------------------
+parse_first_token:
+mov cx, 1
+mov si, arg_string.data - 1
+call parse_next_token
+ret
+
+;-------------------------------------------------------------------------------
 ; Advances SI/CX to point to the next token in the argument string.
 ;
 ; Call with CX=0 to start parsing from the beginning.
@@ -226,28 +237,33 @@ ret
 ; Clobbers AX.
 ;-------------------------------------------------------------------------------
 parse_next_token:
-cmp cx, 0
-jne .skip_init
-mov si, arg_string.data
-.skip_init:
-add si, cx                              ; Go to character following token
+cmp cx, 0                           ; Make sure we have a previous token before continuing
+je .ret
+
+; Move SI to point to the first char following the previous token.
+; We also repurpose CX within this function: CX = number of bytes remaining.
+add si, cx                          ; Go to character following previous token
 mov cx, arg_string.data
-add cl, byte [arg_string.length]
-sub cx, si                              ; Calculate CX=number of remaining characters, including char pointed at by SI
+add cl, [arg_string.length]         ; CX = pointer to end of string
+sub cx, si                          ; CX = number of remaining characters, including char pointed at by SI
 jle .end_of_string
 
-.skip_loop:             ; Fast-forward past any token-separating characters.
+; Skip SI past any token-separating characters
+.skip_loop:
 mov al, [si]
 call is_token_separator
-jne .skip_break
+jne .token_start
 inc si                  ; Advance to next character
 dec cx
-jz .end_of_string       ; If we run out of characters, quit our token search
-jmp .skip_loop
-.skip_break:
+jnz .skip_loop
+.end_of_string:
+mov cx, 0               ; We hit the end of the string
+ret
 
+; SI = the start of a token. Now find where the token ends.
+.token_start:
 push si
-.count_loop:            ; Advance pointer past all token characters
+.count_loop:
 inc si
 dec cx
 jz .count_break
@@ -257,15 +273,11 @@ je .count_break
 jmp .count_loop
 .count_break:
 
-; We found a token! Calculate its size and return it
+; We found both ends of the token! Calculate its size and return it
 mov cx, si
 pop si
 sub cx, si
-ret
-
-; We ran out of characters without finding a token
-.end_of_string:
-mov cx, 0
+.ret:
 ret
 
 ;-------------------------------------------------------------------------------
