c44515c6dbcb148fc26ac86a0925021c3bcd9a46
Chris Mounce <christophermounce@gmail.com>
2021-05-06 01:41:13 -0700

    Implement initial TSR multiplex handler

--- src/tsr.asm
+++ src/tsr.asm
@@ -1,5 +1,32 @@
 ;; Functions and definitions for TSR-specific stuff
 
+;===============================================================================
+; Constants
+;-------------------------------------------------------------------------------
+; Calculates the 32-bit FNV-1a hash of a string and assigns it to a macro variable.
+; Usage: fnv_hash variable_to_assign, 'string to be hashed'
+%macro fnv_hash 2
+%strlen %%num_bytes %2
+%assign %%hash 0x811c9dc5
+%assign %%i 0
+%rep %%num_bytes
+    %assign %%i %%i+1
+    %substr %%byte %2 %%i
+    %assign %%hash ((%%hash ^ %%byte) * 0x01000193) & 0xFFFFFFFF
+%endrep
+%assign %1 %%hash
+%endmacro
+
+; Calculate a somewhate-unique 32-bit identifier for our TSR
+%define TSR_ID_STRING "Quantum's all-purpose TSR, version rewrite-in-progress"
+fnv_hash TSR_ID_HASH, TSR_ID_STRING
+TSR_HASH_LO equ TSR_ID_HASH & 0xFFFF
+TSR_HASH_HI equ TSR_ID_HASH >> 16
+
+
+;===============================================================================
+; Resident header: statically-allocated variables
+;-------------------------------------------------------------------------------
 ; When processing an interrupt, we reuse the PSP's command-line space as a
 ; miniature stack. The last word of the PSP holds the old stack pointer,
 ; and the 126 bytes preceding it are our temporary stack space.
@@ -11,8 +38,6 @@ old_stack_pointer:
 ; the space will be reused for keeping track of a few variables needed
 ; by the resident code.
 absolute 100h
-
-tsr_id_hash: resb 4
 
 old_int_10h:    ; previous video interrupt
 .offset:    resb 2
@@ -30,6 +55,9 @@ palette_offset: resb 2  ; Location of palette data in resident memory
 ; End of statically-allocated data; generated resident code follows.
 tsr_code_start:
 
+;===============================================================================
+; Resident code and code fragments
+;-------------------------------------------------------------------------------
 segment .text
 ; TODO for full install routine:
 ; 0. Make sure TSR can be installed
@@ -127,6 +155,20 @@ mov dx, [palette_offset]
 ; CX = Size in bytes
 ; Stack: Offset of resident palette
 ;-------------------------------------------------------------------------------
+; Brainstorming an API: What data does this function need?
+;   - Blob of resident code/data to install (pointer/length, SI/CX)
+;   - TSR multiplex ID (AX)
+;   - Location of data tables in the blob (BX) (conflict, used for jmp)
+;   - Location of int 10h in the blob   (CX) (conflict)
+;   - Location of int 2fh in the blob   (DX)
+; Data tables can be chained together. The first routine gets a pointer
+; directly to its data. As part of processing, it advances the pointer to the
+; byte following the end of its data -- setting things up for the next routine.
+;
+; The need to pass in locations of 10h/2fh could be eliminated with some
+; cleverness: 2fh's handler is constant length, and if it is placed at the
+; start of the resident code, 10h would have a known location as well.
+; It feels a little inelegant, though.
 finalize_install:
 ; Copy TSR code into place
 pop bx
@@ -147,8 +189,13 @@ mov     dx, tsr_code_start  ; TODO: This will need to be more sophisticated in t
 int     21h
 sti
 
+; Free environment block before exiting
+mov ah, 49h
+mov es, [2ch]   ; Environment segment from PSP
+int 21h
+
 ; Terminate and stay resident
-mov ax, 3100h
+mov ax, 3100h   ; TSR, return code 0
 pop dx          ; Convert the size in bytes
 add dx, tsr_code_start
 add dx, 16 - 1  ; to the size in 16-byte paragraphs,
@@ -157,22 +204,19 @@ shr dx, cl
 int 21h
 .end_of_contents:
 
-
-; Calculates the 32-bit FNV-1a hash of a string and assigns it to a macro variable.
-; Usage: fnv_hash variable_to_assign, 'string to be hashed'
-%macro fnv_hash 2
-%strlen %%num_bytes %2
-%assign %%hash 0x811c9dc5
-%assign %%i 0
-%rep %%num_bytes
-    %assign %%i %%i+1
-    %substr %%byte %2 %%i
-    %assign %%hash ((%%hash ^ %%byte) * 0x01000193) & 0xFFFFFFFF
-%endrep
-%assign %1 %%hash
-%endmacro
-
-%define TSR_ID_STRING "Quantum's all-purpose TSR, version rewrite-in-progress"
-fnv_hash TSR_ID_HASH, TSR_ID_STRING
-segment .data
-tsr_id_hash_value: dd TSR_ID_HASH
+;-------------------------------------------------------------------------------
+; TSR multiplex handler (int 2Fh)
+;
+; Returns the resident code's segment in BX and the TSR hash in CX:DX.
+;-------------------------------------------------------------------------------
+int_2fh_handler:
+cmp ah, [cs:tsr_id_num]
+je .match
+jmp far [cs:old_int_2fh]
+.match:
+mov al, 0ffh        ; Indicate installed status
+mov bx, cs
+mov cx, TSR_HASH_LO
+mov dx, TSR_HASH_HI
+iret
+.end_of_contents:
