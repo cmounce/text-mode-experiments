81f79c47ebaab3f0d110c376789f633d6067f123
Chris Mounce <christophermounce@gmail.com>
2021-12-31 01:38:21 -0800

    Add ramp8.pal, the first 8-color palette

--- src/procedural-palettes.py
+++ src/procedural-palettes.py
@@ -4,6 +4,7 @@ import itertools
 import pathlib
 
 from lib.palette import Palette
+from lib.oklab import to_oklab, to_srgb
 
 PALETTES_DIR = pathlib.Path(__file__).parent.parent / "goodies" / "palettes"
 
@@ -24,6 +25,11 @@ def main():
         palette.reorder(cga)
         with open(PALETTES_DIR / filename, "wb") as f:
             f.write(bytes(palette))
+
+    # Generate some non-reordered palettes
+    ramp8 = generate_ramp8()
+    with open(PALETTES_DIR / 'ramp8.pal', 'wb') as f:
+        f.write(bytes(ramp8))
 
 
 def generate_quasirandom_r3():
@@ -53,6 +59,25 @@ def generate_rgb332():
     it = filter(lambda x: x != (0.0, 0.0, 0.0) and x != (1.0, 1.0, 1.0), it)
     colors = list(it)
     return Palette(colors)
+
+
+def generate_ramp8():
+    """Generates a 8-color ramp"""
+    # Make all 3-bit colors, sorted by brightness
+    off_on = [0.0, 1.0]
+    rgb_colors = list(itertools.product(off_on, off_on, off_on))
+    lab_colors = [to_oklab(rgb) for rgb in rgb_colors]
+    lab_colors.sort()
+
+    # Even out the brightness
+    for i in range(8):
+        brightness = i/7
+        color = lab_colors[i]
+        lab_colors[i] = (brightness, color[1], color[2])
+
+    # Convert to RGB and double the result
+    rgb_colors = [to_srgb(lab) for lab in lab_colors]
+    return Palette(rgb_colors + rgb_colors)
 
 
 if __name__ == "__main__":
